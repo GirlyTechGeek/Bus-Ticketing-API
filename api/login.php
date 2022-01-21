bb52c86a0d7f4453f7e1d3983275cdfc4c6567c1
Anna Asunka <asunkaa41@gmail.com>
2022-01-21 13:08:29 +0000

    vercel

--- api/login.php
+++ api/login.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Content-Type: application/json; charset=utf-8');
+
 require_once("database.php");
 require_once("response.php");
 $writeDB = DB::connectWriteDB();
